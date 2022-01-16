8649db34b2ae1f89d6a390fb7b9c65ed33b5343e
Troyas <troyaszor@gmail.com>
2022-01-16 20:41:36 +0300

    work1

--- HomeWork/index.py
+++ HomeWork/index.py
@@ -1,7 +1,17 @@
 from dataset import users, countries
 from pprint import pprint
 
-users_wrong_password = users.keys()
+users_wrong_password = []
 
-print(users_wrong_password)
+
+for user in users:
+    if 'password' in user:
+        users_wrong_password.append(user['password'])
+    if 'mail' in user:
+        users_wrong_password.append(user['mail'])
+print(users_wrong_password) 
+      
+
+        
+
 
